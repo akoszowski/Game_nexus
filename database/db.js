19cc33c1fc831eb6e38a5df6f91fd88bafd0bb58
akoszowski <a.koszowski@students.mimuw.edu.pl>
2021-06-02 12:18:18 +0200

    Fixed db URL

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = "postgres://fjoiauwcdcgong:8fbf775c79d9cda97e876a56d0a6cda271201f87cbeb2bcf41fb8e9e9f7ad063@ec2-54-155-226-153.eu-west-1.compute.amazonaws.com:5432/dd4hv7g3gngb97";
+const connectionString = "postgres://prfbbrfnnhpglk:06dcbe751adf4522c00afff761b4003bd37a39a55f47ed5b70dd4eb7877f35b2@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d9h8goms7cnmln";
 
 // Connecting to our database via URI
 const client = new Client({
